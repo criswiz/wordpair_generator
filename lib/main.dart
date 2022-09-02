5740f16628846ab4d3ee9a5725648d57330ff833
Samuel Agbenorwu <samuel.agbenorwu@tgholdingsgh.com>
2022-09-02 16:22:05 +0000

    test page render images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './random_words.dart';
+import './test.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,6 +15,6 @@ class MyApp extends StatelessWidget {
             backgroundColor: Color.fromARGB(255, 77, 17, 150),
           ),
         ),
-        home: const RandomWords());
+        home: const SenseiTest());
   }
 }
---
+++ lib/test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SenseiTest extends StatefulWidget {
+  const SenseiTest({Key? key}) : super(key: key);
+
+  @override
+  State<SenseiTest> createState() => _SenseiTestState();
+}
+
+class _SenseiTestState extends State<SenseiTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+      ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 3,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+          return Center(
+            child: Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
